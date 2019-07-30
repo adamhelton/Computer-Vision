4358d91c66c2bef8f793db1b6cb40878dcae1c55
Adam Helton <helton93@protonmail.com>
2019-07-30 14:03:33 -0400

    added resize capability

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import cv2
 
-img = cv2.imread("picture.jpg", 1)
+img = cv2.imread("718_1000.jpg", 1)
+
+
+resized_image = cv2.resize(img,(1000,500))
+cv2.imshow("718_1000", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
